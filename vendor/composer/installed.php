9e1caa93a8ed229f635d78a437f88dbbbbbd1eb8
rajvindergithub <rajvindergithub@gmail.com>
2022-09-12 12:31:24 +0530

    add root files for sql formatter

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'jdorn/sql-formatter' => array(
+            'pretty_version' => 'v1.2.17',
+            'version' => '1.2.17.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../jdorn/sql-formatter',
+            'aliases' => array(),
+            'reference' => '64990d96e0959dff8e059dfcdc1af130728d92bc',
+            'dev_requirement' => false,
+        ),
+    ),
+);
